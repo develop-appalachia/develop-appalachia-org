e50230ea5a01e9074687e7a14482fc3c0280b28c
Bennett, Trevor D <bennett.1430@osu.edu>
2019-06-09 13:48:15 -0400

    basic header footer skeleton

--- develop-appalachia/src/App.js
+++ develop-appalachia/src/App.js
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Headroom from "react-headroom";
+import FooterPage from "./footer";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <body>
+        <Headroom>
+          {" "}
+          <h1>Develop Appalachia</h1>
+        </Headroom>
+        <div className="BigBox">hello world</div>
+        <FooterPage />
+      </body>
     </div>
   );
 }
